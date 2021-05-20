36c2d4910505c422e27d5a033b7183c0ebb8ba6a
loftyincltd <84028325+loftyincltd@users.noreply.github.com>
2021-05-20 14:01:50 +0100

    Update api.js
    added https://www.sharowmip.tk/ to auth file

--- src/redux/authentication/api.js
+++ src/redux/authentication/api.js
@@ -12,7 +12,7 @@ const api = (store) => (next) => async (action)  => {
     callback: function(){};
     try {
       const response = await axios.post(
-        'http://3.8.182.114:3003/v1' + url,
+        'https://www.sharowmip.tk/v1' + url,
         body
       ); 
       return callback(false, response);
@@ -39,7 +39,7 @@ const api = (store) => (next) => async (action)  => {
     }
     try {
       const response = await axios.get(
-        'http://3.8.182.114:3003/v1' + url, config
+        'https://www.sharowmip.tk/v1' + url, config
        
       ); 
       return callback(false, response);
